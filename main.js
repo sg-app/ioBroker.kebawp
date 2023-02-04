b6486f8eb84e6dd39334f6fed51192f04b62e475
root <root@hcsdev.fritz.box>
2023-02-04 14:50:05 +0000

    Initial Version

--- main.js
+++ main.js
@@ -9,7 +9,9 @@
 const utils = require("@iobroker/adapter-core");
 
 // Load your modules here, e.g.:
-// const fs = require("fs");
+const hp = require("./lib/heatingpump");
+const fetch = require("node-fetch");
+const timeouts = require('timers/promises');
 
 class Kebawp extends utils.Adapter {
 
@@ -23,9 +25,8 @@ class Kebawp extends utils.Adapter {
 		});
 		this.on("ready", this.onReady.bind(this));
 		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
+
 	}
 
 	/**
@@ -33,59 +34,76 @@ class Kebawp extends utils.Adapter {
 	 */
 	async onReady() {
 		// Initialize your adapter here
+		try {
+			// Reset the connection indicator during startup
+			this.setState("info.connection", false, true);
 
-		// Reset the connection indicator during startup
-		this.setState("info.connection", false, true);
+			// The adapters config (in the instance object everything under the attribute "native") is accessible via
+			// this.config:
+			this.log.info("config IPAddress: " + this.config.IPAddress);
+			this.log.info("config Polltime: " + this.config.Polltime);
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config IPAddress: " + this.config.IPAddress);
-		this.log.info("config Polltime: " + this.config.Polltime);
+			if (!this.config.IPAddress.match("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}")) {
+				this.log.error("Keine gültige IP Adresse.");
+				return;
+			}
 
+			// Erstelle benötigte Adapter Struktur.
+			this.createAllStates();
+
+			// Starte Hauptroutine.
+			this.updateData();
+
+			this.setState("info.connection", true, true);
+		} catch (err) {
+			this.log.error("Fehler beim Starten von adapter: " + err.message);
+		}
 		/*
 		For every state in the system there has to be also an object of type state
 		Here a simple template for a boolean variable named "testVariable"
 		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
 		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
+		// await this.setObjectNotExistsAsync("testVariable", {
+		// 	type: "state",
+		// 	common: {
+		// 		name: "testVariable",
+		// 		type: "boolean",
+		// 		role: "indicator",
+		// 		read: true,
+		// 		write: true,
+		// 	},
+		// 	native: {},
+		// });
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
+		// // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
+		// this.subscribeStates("testVariable");
+		// // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
+		// // this.subscribeStates("lights.*");
+		// // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
+		// // this.subscribeStates("*");
 
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+		// /*
+		// 	setState examples
+		// 	you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+		// */
+		// // the variable testVariable is set to true as command (ack=false)
+		// await this.setStateAsync("testVariable", true);
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
+		// // same thing, but the value is flagged "ack"
+		// // ack should be always set to true if the value is received from or acknowledged from the target system
+		// await this.setStateAsync("testVariable", { val: true, ack: true });
 
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+		// // same thing, but the state is deleted after 30s (getState will return null afterwards)
+		// await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
 
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
+		// // examples for the checkPassword/checkGroup functions
+		// let result = await this.checkPasswordAsync("admin", "iobroker");
+		// this.log.info("check user admin pw iobroker: " + result);
 
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+		// result = await this.checkGroupAsync("admin", "admin");
+		// this.log.info("check group user admin group admin: " + result);
+		// this.log.debug(hp.getAllParams);
+
 	}
 
 	/**
@@ -95,11 +113,6 @@ class Kebawp extends utils.Adapter {
 	onUnload(callback) {
 		try {
 			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
-
 			callback();
 		} catch (e) {
 			callback();
@@ -138,23 +151,123 @@ class Kebawp extends utils.Adapter {
 		}
 	}
 
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
+	/**
+	 * Hauptroutine Datenabruf von Wärmepumpe.
+	 */
+	async updateData() {
+		this.log.info("Start Cycle to collect Data.");
+		try {
+			// Hole alle Daten und update die states.
+			var data = await this.getWpData();
+			this.log.debug("Received data: " + JSON.stringify(data));
+			await this.updateAllStates(data);
+			this.log.debug("Cycle is done.");
+			await timeouts.setTimeout(parseInt(this.config.Polltime) * 1000);
+			this.updateData();
+		}
+		catch (err) {
+			this.log.error(err.message);
+		}
+	}
 
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
+	/**
+	 * Get the WP Data.
+	 * @returns {Promise<any>} Return JSON Object form Request.
+	 */
+	async getWpData() {
+		this.log.debug("Start Fetch Request.");
+		const response = await fetch(
+			"http://" + this.config.IPAddress + "/var/readWriteVars?languageCode=de",
+			{
+				method: 'Post',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(hp.getAllParamNames)
+			}
+		);
+		return response.json(); // Extracting data as a JSON Object from the response
+	}
+
+	/**
+	 * Create all needed states.
+	 */
+	async createAllStates() {
+		this.log.debug("Create all statess");
+		for (const obj of hp.AllParams) {
+			this.log.silly(`create state: ${obj.sid}.${obj.name}`);
+
+			switch (obj.type) {
+				case 'number':
+					await this.setObjectNotExistsAsync(`${obj.sid}.${obj.friendlyName}`, {
+						common: {
+							name: obj.friendlyName,
+							role: "value",
+							write: false,
+							read: true,
+							type: "number",
+							unit: obj.unit
+						},
+						type: 'state',
+						native: {}
+					});
+					break;
+				case 'boolean':
+					await this.setObjectNotExistsAsync(`${obj.sid}.${obj.friendlyName}`, {
+						common: {
+							name: obj.friendlyName,
+							role: "state",
+							write: false,
+							read: true,
+							type: "boolean"
+						},
+						type: 'state',
+						native: {}
+					});
+					break;
+				default:
+					await this.setObjectNotExistsAsync(`${obj.sid}.${obj.friendlyName}`, {
+						common: {
+							name: obj.friendlyName,
+							role: "state",
+							write: false,
+							read: true,
+							type: "string"
+						},
+						type: 'state',
+						native: {}
+					});
+					break;
+			}
+		}
+	}
+
+	/**
+	 * Updates all states.
+	 * @param {object} data JSON Data Array
+	 */
+	async updateAllStates(data) {
+		// Zahlen und Boolen zum richtigen Typen konvertieren!
+		try {
+			this.log.debug("Start with Update all States.");
+			for (var i = 0; i < data.length; i++) {
+				var obj = data[i];
+				for (var prop in obj) {
+					if (obj.hasOwnProperty(prop) && (!isNaN(obj[prop]) || obj[prop] === "false" || obj[prop] === "false")) {
+						obj[prop] = JSON.parse(obj[prop]);
+					}
+				}
+			}
+
+			for (const obj of data) {
+				var item = hp.AllParams.find(f => f.name === obj.name);
+				if (item != undefined) {
+					this.log.silly(`update state: ${item.sid}.${item.friendlyName} mit Value: ${obj.value}`);
+					await this.setStateAsync(`${item.sid}.${item.friendlyName}`, obj.value, true);
+				}
+			}
+		} catch (err) {
+			this.log.error("Can't update states. " + err.message);
+		}
+	}
 
 }
 
